45bbf600967da485ae7469d72f8e53f5cc0294ec
Tararkov Andrey <gfhflbuvf46@mail.ru>
2018-08-07 16:59:04 +0500

    Default Changelist

---
+++ src/main/java/org/App.java
@@ -0,0 +1,23 @@
+package org;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Hello world!
+ */
+public class App extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        PrintWriter out = resp.getWriter();
+        out.print("<h1>Hello Servlet</h1>");
+
+    }
+}
