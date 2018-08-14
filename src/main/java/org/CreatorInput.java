5c32ee31b051542f9924ce2226c8e057fd6b435a
Tararkov Andrey <gfhflbuvf46@mail.ru>
2018-08-14 09:56:48 +0500

    Временный коммит

--- src/main/java/org/CreatorInput.java
+++ src/main/java/org/CreatorInput.java
@@ -2,6 +2,7 @@ package org;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class CreatorInput extends AbstractPageFactory {
 
@@ -10,6 +11,9 @@ public class CreatorInput extends AbstractPageFactory {
     }
 
     public void build() throws Exception {
+        HttpSession session = page.getValue().getRequest().getSession();
+        OperationsHistory operationsHistory = new OperationsHistory();
+        page.getValue().getRequest().setAttribute("operationsHistory", operationsHistory.getHistory(session));
         page.getValue().getRequest().getRequestDispatcher("input.jsp").forward(page.getValue().getRequest(), page.getValue().getResponse());
     }
 }
